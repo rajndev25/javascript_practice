1054e51a944c9ee6f4183224607a36b39c6a4a04
Rajan Kumar <115071252+rajndev25@users.noreply.github.com>
2023-05-16 12:25:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Chaliye shuru karte hai');
+
+//object create
+
+let rectangle = {
+    lenghth: 1,
+    breadth: 2,
+
+    draw: function() {
+        console.log('drawing rectangle');
+    }
+    
+};
